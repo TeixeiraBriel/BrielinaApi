c5f6f94bcbabdfd0c6a8a9f74c3d5db531edcdb5
Gabriel Teixeira <biel.gt@gmail.com>
2022-01-18 22:35:25 -0300

    Include Projeto RPGCsharpDB

--- Infraestrutura/ConnectionString.cs
+++ Infraestrutura/ConnectionString.cs
@@ -31,5 +31,19 @@ namespace Infraestrutura
 
             return caminho;
         }
+
+        public string carregarConnectionStringRpgCSharp(string id = "RpgCSharp")
+        {
+            var path = System.IO.Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+
+            var caminho = ConfigurationManager.ConnectionStrings[id].ConnectionString;
+
+            path = path.Replace("file:\\", "");
+
+            caminho = caminho.Replace("|Diretorio|", path);
+
+            return caminho;
+        }
     }
 }
---
+++ Infraestrutura/Entidades/FragmentoRpg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.Entidades
+{
+    public class FragmentoRpg
+    {
+        public int IdFragmentoHistoria { get; set; }
+        public string TituloFragmento { get; set; }
+        public string TextoFragmento { get; set; }
+        public int IdFragmentoParent { get; set; } = 0;
+
+    }
+}
