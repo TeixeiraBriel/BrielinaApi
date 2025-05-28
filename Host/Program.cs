5ca6950300e8e2de43d02f1f05b3bcc778bb8536
Gabriel Teixeira <biel.gt@gmail.com>
2025-05-27 23:36:14 -0300

    Add CORS

--- Host/Program.cs
+++ Host/Program.cs
@@ -13,6 +13,20 @@ builder.Services.AddSwaggerGen();
 //Dependency Injection
 _ = new Bootstrapper(services, configuration);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:4200", "https://rpga-ngular-frontend.vercel.app")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,9 +41,10 @@ if (app.Environment.IsDevelopment() && app.Configuration["ASPNETCORE_HTTPS_PORT"
     app.UseHttpsRedirection();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
 
 app.MapControllers();
 
-//app.MapGet("/", () => "API Brielina rodando!");
 app.Run();
